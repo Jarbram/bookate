c24c540c56ebdd1c57ddae428ced217013c53729
Jarbram <huacchillocastillo@gmail.com>
2025-05-27 14:48:10 -0500

    add the code for ads

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import theme from '../theme/theme';
 import { League_Spartan } from 'next/font/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useEffect } from 'react';
 
 import { Providers } from './providers';
 
@@ -16,11 +17,21 @@ const leagueSpartan = League_Spartan({
 const queryClient = new QueryClient()
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    try {
+      const script = document.createElement('script');
+      script.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_CLIENT}`;
+      script.async = true;
+      script.crossOrigin = 'anonymous';
+      document.head.appendChild(script);
+    } catch (error) {
+      console.error('Error al cargar AdSense:', error);
+    }
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <html lang="es" className={leagueSpartan.className}>
-        <head>
-        </head>
         <body>
           <Providers>{children}</Providers>
         </body>
