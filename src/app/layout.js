fadeb54403fdcdb71ace0a16fa14108efa58212d
Jarbram <huacchillocastillo@gmail.com>
2025-05-27 12:22:00 -0500

    add the code for ads

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import theme from '../theme/theme';
 import { League_Spartan } from 'next/font/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Script from 'next/script';
 
 const leagueSpartan = League_Spartan({
   subsets: ['latin'],
@@ -17,6 +18,14 @@ export default function RootLayout({ children }) {
   return (
     <QueryClientProvider client={queryClient}>
       <html lang="es" className={leagueSpartan.className}>
+        <head>
+          <Script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8583192861201767"
+            crossOrigin="anonymous"
+            strategy="lazyOnload"
+          />
+        </head>
         <body>
           {children}
         </body>
